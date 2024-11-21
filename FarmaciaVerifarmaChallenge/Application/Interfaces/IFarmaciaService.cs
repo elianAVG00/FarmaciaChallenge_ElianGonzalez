eee8b618501eacc9a5eb201ec981484cf6b3fb7c
elianAVG00 <elian.avgonzalez@hotmail.com>
2024-11-21 19:22:56 -0300

    Agrego Service

---
+++ FarmaciaVerifarmaChallenge/Application/Interfaces/IFarmaciaService.cs
@@ -0,0 +1,18 @@
+﻿using FarmaciaVerifarmaChallenge.Domain.Entities;
+
+namespace FarmaciaVerifarmaChallenge.Application.Interfaces
+{
+    public interface IFarmaciaService
+    {
+        public void AddFarmacia(Farmacia farmacia);
+
+        public Farmacia GetFarmaciaById(int farmaciaId);
+
+        public void DeleteFarmacia(int farmaciaId);
+
+        public void UpdateFarmacia(Farmacia farmacia);
+
+        public IEnumerable<Farmacia> GetFarmacias();
+
+    }
+}
---
+++ FarmaciaVerifarmaChallenge/Application/Services/FarmaciaService.cs
@@ -0,0 +1,45 @@
+﻿using FarmaciaVerifarmaChallenge.Application.Interfaces;
+using FarmaciaVerifarmaChallenge.Domain.Entities;
+
+namespace FarmaciaVerifarmaChallenge.Application.Services
+{
+    public class FarmaciaService : IFarmaciaService
+    {
+        private readonly IFarmaciaRepository _farmaciaRepository;
+
+        public FarmaciaService(IFarmaciaRepository farmaciaRepository)
+        {
+            this._farmaciaRepository = farmaciaRepository;
+        }
+
+        public void AddFarmacia(Farmacia farmacia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFarmacia(int farmaciaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Farmacia GetFarmacia(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Farmacia GetFarmaciaById(int farmaciaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Farmacia> GetFarmacias()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFarmacia(Farmacia farmacia)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
